87c9894f452fde01c9f009c165e274178b1ebc9f
Jane <spiralm63@gmail.com>
2013-10-12 17:00:04 -0500

    Done with this challenge #150

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,23 @@ require 'date'
 class Student < ActiveRecord::Base
 # implement your Student model here
 
-# instructions:
+# code in this section was added later in the challenge, for "Validation Magic"
+# Instructions:
+#   Add validations to the Student model so that a student cannot be saved unless the following requirements are met:
+#   -- Email addresses must contain at least one @ character and one . character, with at least one character before the @, one character between the @ and first ., and at least two characters after the final ..
+#   -- Email addresses must be unique across all students.
+#   -- Students must be at least 5 years old.
+
+  validates :email, uniqueness: true, format: { with: /\w+@.\w+.{2,}/ }
+  validates :age, :numericality => { :greater_than => 4 }
+
+# More Advanced Validations
+# Add another validation to the Student model so that a student cannot be saved unless the following requirement is met:
+#   -- Phone numbers must contain at least 10 digits, excluding non-numeric characters.
+validates :phone, length: { minimum: 10 , tokenizer: lambda { |str| str.scan(/\d/) }}
+
+
+# instructions (these were actually the first things modified in file:
 # Create a class called Student (in the app/models/student.rb file) that meets the following requirements:
 #    -- Given a Student model object, I should be able to easily retrieve her full name via a name method.
 #    -- Given a Student model object, I should be able to easily know her age (in years) via an age method.
@@ -28,7 +44,6 @@ class Student < ActiveRecord::Base
       age = today.year - birthday.year
     end
   end
-
 end
 
 
